529e0f1c1ea02b84c72be875db09670d0071fc4d
Bibek Hanal <bbekhamal26@gmail.com>
2025-06-07 22:13:22 +1000

    added css for styling

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,18 @@ function App() {
     { id: 4, name: "corn", calories: 30 },
     { id: 5, name: "brocolli", calories: 25 },
   ];
+     const foods = [
+    { id: 1, name: "pizza", calories: 600 },
+    { id: 2, name: "pasta", calories: 750 },
+    { id: 3, name: "burger", calories: 350 },
+    { id: 4, name: "steak", calories: 477 },
+    { id: 5, name: "rolls", calories: 252 },
+  ];
   return (
     <>
-      <List items={fruits} category = "fruits">Fruits</List>
+      <List items={fruits} category = "fruits"></List>
       <List items = {vegetables} category = "vegetables"/>
+      <List items = {foods} category = "foods"/>
     </>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,16 +1,13 @@
 const List = (props) => {
-
-    const itemList = props.items;
-    const category = props.category;
-
-
+  const itemList = props.items;
+  const category = props.category;
 
   // fruits.sort((a,b)=> a.name.localeCompare(b.name)); //Alphabetical
   // fruits.sort((a,b) => b.name.localeCompare(b.name)); Reverese alphabetical
   // fruits.sort((a,b) => a.calories - b.calories); numeric
   // fruits.sort((a,b) => b.calories - a.calories); reverse numeric
 
-//   const lowCalFruits = itemList.filter((item) => item.calories < 97); //using filter method to compare 
+  //   const lowCalFruits = itemList.filter((item) => item.calories < 97); //using filter method to compare
 
   const listItems = itemList.map((item) => (
     <li key={item.name}>
@@ -19,9 +16,11 @@ const List = (props) => {
   ));
 
   return (
-  <>
-  <h3>{category}</h3>
-  <ol>{listItems}</ol></>);
+    <>
+      <h3 className="list-category">{category}</h3>
+      <ol className="list-items">{listItems}</ol>
+    </>
+  );
 };
 
 export default List;
